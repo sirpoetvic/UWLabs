ceff5494e1c313f09b59a1dadf5b65716e45bd0b
sirpoetvic <86131430+sirpoetvic@users.noreply.github.com>
2022-03-23 18:45:51 -0700

    Implemented part of the monthlyExpenses method.

--- BudgeterUWLab_Folder/BudgeterUWLab.java
+++ BudgeterUWLab_Folder/BudgeterUWLab.java
@@ -1,28 +1,50 @@
 import java.util.Scanner;
 public class BudgeterUWLab {
   
-  Scanner kb = new Scanner(System.in);
-  
+  Scanner sc = new Scanner(System.in);
+  public static int incomeTotal;
+  public static int incomeTotalDaily;
+  public static int expensesTotal;
+  public static int expensesTotalDaily;
+  public static final int DAYS_IN_MONTH = 31;
   
   public static void main(String[] args) {
-    Introductor();
+    intro();
+    income();
+
     
   
   }
   
-  public static void Introductor() {
+  public static void intro() {
     System.out.println("This program asks for your monthly income and expenses, then tells you your net monthly income.");
     
   }
   
-  public static double income() {
+  public static void income() {
+    System.out.println("How many categories of income?");
+    int incomeCategories = sc.nextInt();
+    
+    for(int i = 0; i < incomeCategories; i++) {
+    incomeTotal += nextDouble();
+    }
+
+  }
+
+  public static String getIncome() {
+    return "Total Income = $" + incomeTotal;
+  }
+  
+  public static void monthlyExpenses() {
+    System.out.println("Enter 1) monthly or 2) daily expenses? ");
+    
+    
     
     
   }
-  
-  public static double monthlyExpenses() {
-    
-    
+
+  public static String getMontlyExpenses() {
+    return "Total expenses = $" + expensesTotal + "($ "+ ")";
   }
   
   public static double dailyExpenses() {
@@ -38,9 +60,7 @@ public class BudgeterUWLab {
   public static double netIncome() {
     
     
-  }
-  
-  
+  } 
   
 
 }
