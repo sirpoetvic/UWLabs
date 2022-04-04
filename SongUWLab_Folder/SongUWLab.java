fd9e928a0fc69ce3354f3e64b9b6bb0731991d0f
sirpoetvic <victor.wong88@hotmail.com>
2022-04-03 17:59:32 -0700

    a

--- SongUWLab_Folder/SongUWLab.java
+++ SongUWLab_Folder/SongUWLab.java
@@ -25,17 +25,20 @@ public class SongUWLab {
         stanza7();
     }
 
+    //prints out the first stanza
     public static void stanza1 () {
         System.out.println("There was an old woman who swallowed a fly,");
         lastLines();
     }
     
+    // prints out the second stanza
     public static void stanza2 () {
         System.out.println("There was an old woman who swallowed a spider,");
         System.out.println("That wriggled and iggled and jiggled inside her.");
         lastLines();
     }
 
+    //prints out the third stanza
     public static void stanza3 () {
         System.out.println("There was an old woman who swallowed a bird,");
         System.out.println("How absurd to swallow a bird.");
@@ -44,6 +47,7 @@ public class SongUWLab {
         lastLines();
     }
     
+    //prints out the fourth stanza
     public static void stanza4 () {
         System.out.println("There was an old woman who swallowed a cat,");
         System.out.println("Imagine that to swallow a cat.");
@@ -53,6 +57,7 @@ public class SongUWLab {
         lastLines();
     }
 
+    //prints out the fifth stanza
     public static void stanza5 () {
         System.out.println("There was an old woman who swallowed a dog,");
         System.out.println("What a hog to swallow a dog.");
@@ -63,6 +68,7 @@ public class SongUWLab {
         lastLines();
     }
 
+    //prints out the sixth stanza
     public static void stanza6 () {
         System.out.println("There was an old woman who swallowed a tiger,");
         System.out.println("She thought it was cake, but it was a snake.");
@@ -74,36 +80,43 @@ public class SongUWLab {
         lastLines();
     }
 
+    //prints out the seventh stanza
     public static void stanza7 () {
         System.out.println("There was an old woman who swallowed a horse,");
         System.out.println("She died of course.");
     }
 
+    //prints out why the lady swallowed a spider
     public static void swallowSpider() {
         System.out.println("She swallowed the spider to catch the fly,");
     }
-
+    //prints out why the lady swallowed a bird
     public static void swallowBird() {
         System.out.println("She swallowed the bird to catch the spider,");
     }
 
+    //prints out why the lady swallowed a cat
     public static void swallowCat() {
         System.out.println("She swallowed the cat to catch the bird,");
     }
 
+    //prints out why the lady swallowed a dog
     public static void swallowDog() {
         System.out.println("She swallowed the dog to catch the cat,");
     }
-
+    
+    //prints out why the lady swallowed a tiger
     public static void swallowTiger() {
         System.out.println("She swallowed the tiger to catch the dog,");
     }
 
+    //prints out why the lady swallowed a horse
     public static void swallowHorse() {
         System.out.println("There was an old woman who swallowed a horse,");
         System.out.println("She died of course.");
     }
 
+    //prints out the last line of all of the stanzas
     public static void lastLines() {
         System.out.println("I don't know why she swallowed that fly,");
         System.out.println("Perhaps she'll die.");
