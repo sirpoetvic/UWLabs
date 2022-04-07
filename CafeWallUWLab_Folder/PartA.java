6a98af21f2d4745d53c75954db6141bbefba0fdc
sirpoetvic <victor.wong88@hotmail.com>
2022-04-06 23:50:48 -0700

    Finished Part A of CafeLab, began Part B.

--- CafeWallUWLab_Folder/PartA.java
+++ CafeWallUWLab_Folder/PartA.java
@@ -1,5 +1,47 @@
+//Victor Wong 
+//April 6th, 2022
+//CSE142
+//TA: TA Name
+//Take-Home Assessment #3, Part A
+//
+//This program is intended to produce a cube and on the primary side shown
+//to the user, a circle and triangle are produced. This program
+//depicts accurate ability of use of a GUI (Graphical User Interface)
 package CafeWallUWLab_Folder;
-
+import java.awt.*;
 public class PartA {
+      public static void main(String[] args) {
+        DrawingPanel draw = new DrawingPanel(1000, 1000);
+        Graphics graphics = draw.getGraphics();
+        //triangle
+        //3 pts
+        //(600, 400), (450, 100), (400, 400)
+        graphics.setColor(Color.ORANGE);
+        graphics.drawLine(600, 400, 300, 400);
+        graphics.drawLine(600, 400, 450, 100);
+        graphics.drawLine(450, 100, 300, 400);
+
+        //other lines that make the cube
+        graphics.setColor(Color.RED);
+        graphics.drawLine(600, 100, 700, 0);
+        graphics.drawLine(300, 100, 400, 0);
+        graphics.drawLine(700, 0, 400, 0);
+        graphics.drawLine(600, 400, 700, 300);
+        graphics.drawLine(700, 0, 700, 300);
+
+        //circle, top left corner in (300, 100)
+        graphics.setColor(Color.GREEN);
+        graphics.drawOval(300, 100, 300, 300);
+
+        
+
+        //square, top left corner in (300, 100)
+        graphics.setColor(Color.BLUE);
+        graphics.drawRect(300, 100, 300, 300);
+
+
+        
+      }
+
     
 }
--- CafeWallUWLab_Folder/PartB.java
+++ CafeWallUWLab_Folder/PartB.java
@@ -1,5 +1,34 @@
+//Victor Wong 
+//April 6th, 2022
+//CSE142
+//TA: TA Name
+//Take-Home Assessment #3, Part B
+//
+//This program is intended to produce a depiction of the "Cafe Wall" illusion
+//in the DrawingPanel GUI.  
 package CafeWallUWLab_Folder;
-
+import java.awt.*;
 public class PartB {
-    
+    static DrawingPanel draw = new DrawingPanel(1000, 1000);
+    static Graphics graphics = draw.getGraphics();
+    static int MORTAR = 2;
+    public static void main(String[] args) {
+        
+    }
+
+    public static void boxPairMaker(int sideLength, int x, int y) {
+        graphics.drawRect(x, y, sideLength, sideLength);
+        graphics.drawLine(x, y, x + sideLength, y + sideLength);
+        graphics.drawLine(x, y + sideLength, x + sideLength, y);
+    }
+
+    //generates a row of xBox and whiteBox based on parameters
+    public static void rowMaker(int rowLength) {
+         
+    }
+
+    public static void gridMaker() {
+        //use rowMaker method
+    }
+
 }
